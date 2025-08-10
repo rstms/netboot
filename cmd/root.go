1cf97db01b6620f752e7a3035dec3b11ea09535c
Matt Krueger <mkrueger@rstms.net>
2025-08-10 10:16:07 -0600

    Bump version: 0.0.5 → 0.0.6

--- cmd/root.go
+++ cmd/root.go
@@ -42,7 +42,7 @@ var cfgFile string
 const ProgramName = "netboot"
 
 var rootCmd = &cobra.Command{
-	Version: "0.0.5",
+	Version: "0.0.6",
 	Use:     "netboot",
 	Short:   "netboot server daemon",
 	Long: `
