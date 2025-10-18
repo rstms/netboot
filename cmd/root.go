20b3728d44e00b5b90b0582227292e0352009fa4
Matt Krueger <mkrueger@rstms.net>
2025-10-18 13:22:34 -0600

    Bump version: 1.1.11 → 1.1.12

--- cmd/root.go
+++ cmd/root.go
@@ -38,7 +38,7 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Version: "1.1.11",
+	Version: "1.1.12",
 	Use:     "netboot",
 	Short:   "netboot server ",
 	Long: `
--- server/server.go
+++ server/server.go
@@ -19,7 +19,7 @@ import (
 	"time"
 )
 
-const Version = "1.1.11"
+const Version = "1.1.12"
 
 const DEFAULT_SERVER_NAME = "localhost"
 const DEFAULT_BIND_ADDRESS = "127.0.0.1"
